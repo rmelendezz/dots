20773ba2ff6dc417a930950e0e82d10cce43c846
Toniiz <toniiz@tuta.io>
2024-08-19 13:44:17 -0700

    modify i3 and polybar config, multiple display now shows proper workspace names

---
+++ configs/i3wm_g/Lenovox1Gen5/README.md
@@ -0,0 +1,5 @@
+### i3wm multiple display
+There are multiple ports on this laptop, thus the only way to make the dual screen worked was to set up specific binds to specefic outputs
+
+*config_DP-1* - Shuold be use when a USB-C to Display Port is connected on Port-1 (Closest to the screen)
+
--- configs/polybar_g/Lenovox1Gen5/launch.sh
+++ configs/polybar_g/Lenovox1Gen5/launch.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
-
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
-polybar barra1 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload barra1 &
+    MONITOR=$m polybar --reload barra2 &
+done
