fedcbaebf72d318bd2644cbff5033d68ec41e33f
Toniiz <toniiz@tuta.io>
2024-08-19 20:11:05 -0700

    add flashfocus config, add minimal config for picom, tweak polybar config colors and others

---
+++ configs/picom_l/README.md
@@ -0,0 +1,3 @@
+## Picom configs
+*picom_default.conf* - Default config not much has been changed/editted
+*picom.conf* - Minimal config, with just a few options, no animation, and includes `flashfocus` needed options
--- configs/polybar_g/Lenovox1Gen5/launch.sh
+++ configs/polybar_g/Lenovox1Gen5/launch.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload barra1 &
     MONITOR=$m polybar --reload barra2 &
