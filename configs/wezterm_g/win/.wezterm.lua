be61b17fd62cc72eb7525bd28b1397bf6a59c6fb
toniiz <toniiz@proton.me>
2025-03-08 00:00:52 -0800

    mv: .wezterm to its respective folder

--- configs/wezterm_g/win/.wezterm.lua
+++ configs/wezterm_g/win/.wezterm.lua
@@ -1,38 +1,21 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-
--- Set PWSH.exe as default on start
-config.default.prog = { 'powershell' } -- if on windows 10 replace for 'pwsh.exe'
-
--- Font settings
-config.font = wezterm.font 'FantasqueSansM Nerd Font'
+config.color_scheme = 'GruvboxDark'
+config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
 config.font_size = 16
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.default_domain = 'WSL:Arch'
+config.audible_bell = 'Disabled'
 
 -- Window padding
 config.window_padding = {
-  left = 18,
-  right = 18,
+  left = 24,
+  right = 24,
   top = 8,
   bottom = 8,
 }
 
--- Top bar settings
-config.hide_tab_bar_if_only_one_tab = true
 
--- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-
-
--- and finally, return the configuration to wezterm
 return config
